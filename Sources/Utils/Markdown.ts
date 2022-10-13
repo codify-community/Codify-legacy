ea4dc30c699c093abee13340daa104f6170b7f90
Nik <yxqsnz@gmail.com>
2022-10-13 17:15:13 -0300

    Feat: `/status` command (#9)
    * Feat!: switch function names to camelCase
    
    Feat: improve logging
    
    * Visual(/criar-changelog): change loading status to ephemeral.
    
    * Feat: `Enviar NPPP` context command
    
    * Feat: Migrate to swc
    
    * Swc: enable minification
    
    * Feat: `/status` command
    
    * Ext: add aditional workspace recommended extensions
    
    * Version: debump for 0.1.0-ualpha

--- Sources/Utils/Markdown.ts
+++ Sources/Utils/Markdown.ts
@@ -1,3 +1,7 @@
 export function codeBlock(lang: string, code: string): string {
   return `\`\`\`${lang}\n${code}\`\`\``;
 }
+
+export function codeSnippet(code: string) {
+  return `\`${code}\``;
+}
