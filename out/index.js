82e9ff59140cb33339af1a20784a958b17fefdd0
yxqsnz <yxqsnz@gmail.com>
2022-10-25 15:48:56 -0300

    feat: update git ignore

--- out/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoic3JjLyIsInNvdXJjZXMiOlsiaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
